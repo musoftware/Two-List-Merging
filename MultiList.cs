bd7f89eb21db2113822de5ad4cce984ab7adb0fa
musoftware <MUGenius@windowslive.com>
2016-11-27 17:08:06 +0200

    first

---
+++ MultiList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM_Speed_V2.Operations
+{
+    class MultiList
+    {
+
+        public List<string> a1 = new List<string>();
+        public List<string> a2 = new List<string>();
+        public List<string> a3 = new List<string>();
+
+        public List<a12Obj> CompleteA2(int countA2eachA1 = 1)
+        {
+            if (a1.Count == 0) return new List<a12Obj>();
+            if (a2.Count == 0) return new List<a12Obj>();
+         
+            List<a12Obj> list = new List<a12Obj>();
+            int a1Index = 0;
+            int a1tmp = 0;
+            foreach (var item in a2)
+            {
+                if (a1tmp == countA2eachA1)
+                {
+                    a1Index++;
+                    a1tmp = 1;
+                }
+                else
+                    a1tmp++;
+                
+                if (a1Index >= a1.Count) a1Index = 0;
+
+                list.Add(new a12Obj() { a1 = a1[a1Index], a2 = item });
+            }
+            return list;
+        }
+
+
+
+
+
+    }
+}
---
+++ a12Obj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AM_Speed_V2.Operations
+{
+    class a12Obj
+    {
+        public string a1 { get; set; }
+        public string a2 { get; set; }
+    }
+}
